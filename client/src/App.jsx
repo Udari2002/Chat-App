73e6e0ee224d1e3513c6db001b07a39223f37943
Moksha <eg245236@engug.ruh.ac.lk>
2025-09-30 09:15:58 +0530

    change the project

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
   return (
     <div className='bg-[url("./src/assets/bgImage.svg")] bg-contain'>
     <Routes>
-      <Route path='/'element={<HomePage />} />
+      <Route path='/' element={<HomePage />} />
       <Route path='/login' element={<LoginPage />} />
       <Route path='/profile' element={<ProfilePage />} />
     </Routes>
--- client/src/pages/LoginPage.jsx
+++ client/src/pages/LoginPage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { authAPI } from '../lib/api';
 import assets from '../assets/assets';
 
 const LoginPage = () => {
