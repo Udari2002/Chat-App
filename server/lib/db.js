755aa19d48dce6b1908e4cd1a479dbfe607f1086
Moksha <eg245236@engug.ruh.ac.lk>
2025-10-09 21:06:58 +0530

    Implement user signup functionality with password hashing and JWT token generation; update MongoDB URI in environment variables

--- server/lib/db.js
+++ server/lib/db.js
@@ -3,8 +3,8 @@ import mongoose from "mongoose";
 //function to connect to mongoDB database
 export const connectDB = async () => {
     try{
-        mongoose.connection.on('connected', () => console.log('Database connected successfully'))
-        await mongoose.connect(`${process.env.MONGO_URI}/chatApp`)
+        mongoose.connection.on('connected', () => console.log('Database connected successfully'));
+        await mongoose.connect(`${process.env.MONGO_URI}/chatApp`);
     }catch(error){
         console.log(error)
 
