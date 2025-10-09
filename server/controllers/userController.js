16054723c2f195ad70a940a17eb69bba902ca0e4
Moksha <eg245236@engug.ruh.ac.lk>
2025-10-09 22:28:27 +0530

    Add initial files for cloudinary configuration, authentication middleware, message model, and user routes

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,6 +1,8 @@
-import { generateToken } from "../lib/utils";
-import User from "../models/user";
+import cloudinary from "../lib/cloudinary.js";
+import { generateToken } from "../lib/utils.js";
+import User from "../models/user.js";
 import bcrypt from "bcryptjs";
+
 
 //signup new user
 export const signup = async (req, res) => {
@@ -34,3 +36,49 @@ export const signup = async (req, res) => {
 }
 
 //Controller for user login
+export const login = async (req, res) => {
+    try {
+        const { email, password} = req.body;
+        const userData = await User.findOne({ email });
+
+        const isPasswordCorrect = await bcrypt.compare(password, userData.password);
+        if (!isPasswordCorrect) {
+            return res.json({ success: false, message: "Invalid Credentials" });
+        }
+        const token = generateToken(userData._id);
+        res.json({ success: true, token, userData, message: "Login Successful" });
+} catch (error) {
+        console.log(error.message);
+        res.json({ success: false, message: error.message });
+    }
+
+}
+
+//controller to check if user is authenticated
+export const checkAuth = async (req, res) => {
+    res.json({ success: true, user: req.user });
+}
+
+//controller to update user profile details
+export const updateProfile = async (req, res) => {
+    try {
+        const { fullName, bio, profilePic } = req.body;
+
+        const userId = req.user._id;
+        let updatedUser;
+
+        if(!profilePic){
+            updatedUser = await User.findByIdAndUpdate(userId, { fullName, bio }, {new: true});
+        }
+        else{
+            const upload = await cloudinary.uploader.upload(profilePic);
+
+            updatedUser = await User.findByIdAndUpdate(userId, { fullName, bio, profilePic: upload.secure_url }, {new: true});
+        }
+        res.json({ success: true, user: updatedUser });
+    }catch (error) {
+        console.log(error.message);
+        res.json({success:false,message:error.message})
+    }
+
+}
