05ceeb5482e8d4d70a6ef0b7b212a771fd989946
Moksha <eg245236@engug.ruh.ac.lk>
2025-10-10 09:08:28 +0530

    Implement message handling: add message model, controller, and routes for sending and retrieving messages

--- server/server.js
+++ server/server.js
@@ -4,10 +4,37 @@ import cors from 'cors';
 import http from 'http';
 import { connectDB} from './lib/db.js';
 import userRouter from './routes/userRoutes.js';
+import messageRouter from './routes/messageRout.js';
+import { Server } from 'socket.io';
 
 //create express app and http server
 const app = express();
 const server = http.createServer(app);
+
+//Initialize socket.io server
+export const io = new Server(server, {
+    cors: {origin: "*"}
+})
+
+//Store online users
+export const UserSocketMap = {};//{userId:socketId}
+
+//Socket.io connection handler
+io.on('connection', (socket) => {
+    const userId = socket.handshake.query.userId;
+    console.log('User connected:', userId);
+
+    if(userId) UserSocketMap[userId] = socket.id;
+
+    //Emit online users to all connected clients
+    io.emit('getOnlineUsers', Object.keys(userSocketMap));
+
+    socket.on('disconnect', () => {
+        console.log('User disconnected:', userId);
+        delete UserSocketMap[userId];
+        io.emit('getOnlineUsers', Object.keys(UserSocketMap));
+    })
+})
 
 //middleware setup
 app.use(cors());//connet url
@@ -16,6 +43,7 @@ app.use(express.json({limit: '4mb'})); // Increased limit for large payloads
 //route setup
 app.use('/api/status', (req, res) => res.send('Server is live!'));
 app.use("/api/auth", userRouter);
+app.use("/api/message", messageRouter);
 
 //connect to mongoDB database
 await connectDB();
